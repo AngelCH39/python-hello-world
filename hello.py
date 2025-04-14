12c1fdfb6582a814212a1a2f902164fd1ccf3249
AngelCH39 <149042745+AngelCH39@users.noreply.github.com>
2025-04-13 20:15:20 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,2 @@
-print("¡Hola mundo desde Github! Ahora estoy conectado a Jenkins :) gatito!!!! perrito!!!! ")
-print("Ahora vengo de un hook")
-print("Conejito!!!")
+print("¡Hola mundo desde Github! Ahora estoy conectado a Jenkins :) ")
+print("Ahora vengo de un hook para actividad 1")
